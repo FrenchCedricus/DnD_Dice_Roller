c9cf6eb78f9ca43eee06a443d55e41646d64c22c
Cédric Moutet <cedric.moutet@lyceeastier.com>
2023-04-15 01:33:15 +0200

    1.0 Done ! Use Random to generate int, sum it and write! 2.0 Will add + than 9 dice hehe

--- DnD_Dice_Roller/Program.cs
+++ DnD_Dice_Roller/Program.cs
@@ -6,7 +6,53 @@ namespace DnD_Dice_Roller // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Random random;
+            int sum = 0;
+            Console.WriteLine("Bonjour, qu'est-ce qu'on tir ?");
+            Console.WriteLine("");
+            while (true)
+            {
+                string response = Console.ReadLine();
+
+                if (response.Length >= 3 && (response.ElementAt(1) == 'd' || response.ElementAt(1) == 'D'))
+                {
+                    int numberOfDice = int.Parse(response.ElementAt(0).ToString());
+                    int rangeOfDice = int.Parse(response.ElementAt(2).ToString());
+                    if (response.Length == 4)
+                    {
+                        rangeOfDice = int.Parse(response.ElementAt(2).ToString() + response.ElementAt(3).ToString());
+                    }
+
+                    for (int i = 0; i < numberOfDice; i++)
+                    {
+                        random = new Random();
+                        int generatedNumber = random.Next(rangeOfDice);
+                        generatedNumber = generatedNumber + 1;
+                        sum = sum + generatedNumber;
+                        if (generatedNumber == 1)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                        }
+                        else if (generatedNumber == 20)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Yellow;
+                        }
+                        else
+                        {
+                            Console.ForegroundColor = ConsoleColor.White;
+                        }
+                        Console.WriteLine(generatedNumber);
+                    }
+                    if (numberOfDice > 1)
+                    {
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.WriteLine("Somme totale : " + sum);
+                    }
+                }
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("--------------------------------");
+            }
+
         }
     }
 }
